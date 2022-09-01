ecac99509399af18ebe4ec4a66ad39f706d40527
GaminnLinux <43827303+GamerHun1238@users.noreply.github.com>
2022-09-01 19:33:14 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
   id("tabtps.parent")
 }
 
+repositories {
+    maven { url "https://repo.dmulloy2.net/repository/public/" }
+}
+
+dependencies {
+    compileOnly group: "com.comphenix.protocol", name: "ProtocolLib", version: "4.7.0";
+}
+
 group = "xyz.jpenilla"
 version = "1.3.16-SNAPSHOT"
 description = "Monitor your server's performance in real time"
